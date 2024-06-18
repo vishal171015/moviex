e228d3322763ee29d773f70b654457d63b4a8002
vaibhav <vaibhavbhalla52@gmail.com>
2024-06-18 20:49:03 +0530

    error fixed

--- src/pagess/404/NoPageFound.jsx
+++ src/pagess/404/NoPageFound.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import "./style.scss";
-
+import ContentWrapper from "../../componentss/contentWrapper/ContentWrapper";
 const NoPageFound = () => {
-  return (
-    <div>NoPageFound</div>
+  
+    return (
+      <div className="pageNotFound">
+          <ContentWrapper>
+              <span className="bigText">404</span>
+              <span className="smallText">Page not found!</span>
+          </ContentWrapper>
+      </div>
+
   )
 }
 
